6ab920561069fd70280b690c2e5174e8da9f976f
Romina Barrionuevo <105323621+Romina-Barrionuevo@users.noreply.github.com>
2023-12-14 20:38:12 -0300

    Add files via upload
    buck up base de datos y proyecto java

---
+++ TRABAJO_FINALCAC_JAVA_BARRIONUEVO/integrador_cac_oradores.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE  IF NOT EXISTS `integrador_cac` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `integrador_cac`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: integrador_cac
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `oradores`
+--
+
+DROP TABLE IF EXISTS `oradores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `oradores` (
+  `id_orador` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `mail` varchar(255) DEFAULT NULL,
+  `tema` varchar(255) DEFAULT NULL,
+  `fecha_alta` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_orador`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oradores`
+--
+
+LOCK TABLES `oradores` WRITE;
+/*!40000 ALTER TABLE `oradores` DISABLE KEYS */;
+INSERT INTO `oradores` VALUES (20,'Jose','Lopez','jose@gmail.com','Salud','2023-12-14 03:00:00'),(21,'Luciano','Fuentes','luciano@gmail.com','Sociedad','2023-12-14 03:00:00'),(32,'Silvia','Gonzalez','silvia@gmail.com','Sociedad','2023-12-14 03:00:00'),(33,'Roberto','Garcia','roberto@gmail.com','Sociedad','2023-12-14 03:00:00'),(35,'Mariana','Gonzalez','mari@gmail.com','Sociedad','2023-12-14 03:00:00'),(36,'Romina','Barrionuevo','ro@gmail.com','Economia','2023-12-14 03:00:00');
+/*!40000 ALTER TABLE `oradores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-14 20:22:52
